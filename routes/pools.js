af45ae937b8eca327af85254d84c9e3d552d487f
Shay Davidson <sdavidson@paypal.com>
2019-01-10 17:55:41 +0200

    added update and delete

--- routes/pools.js
+++ routes/pools.js
@@ -39,6 +39,16 @@ router.get('/:poolId', async function(req, res, next) {
     res.send(result);
 });
 
+router.delete('/:poolId', async function(req, res, next) {
+    const result = await database.Pool.findById(req.params.poolId, poolFetchOptions()).destroy();
+    res.send(result);
+});
+
+router.patch('/:poolId', async function(req, res, next) {
+    const result = await database.Pool.findById(req.params.poolId, poolFetchOptions()).update(req.body);
+    res.send(result);
+});
+
 router.get('/:poolId/contributions', async function(req, res, next) {
     const result = await database.Contribution.findAll({where: {poolId: req.params.poolId}});
     res.send(result);
